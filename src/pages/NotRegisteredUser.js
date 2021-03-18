f51c002df2df98bee2646e4b44f35d49ca363e50
NestorMonroy <nestor.monroy.90@gmail.com>
2021-03-18 13:54:03 -0600

    [add] userForm

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,14 +1,26 @@
 import React, { Fragment } from "react";
 import Context from "../Context";
-import { UserForm } from "../components/userForm";
+import { UserForm } from "../components/UserForm";
+import { RegisterMutation } from "../container/RegisterMutation";
 
 export const NotRegisteredUser = () => (
   <Context.Consumer>
     {({ activateAuth }) => {
       return (
         <Fragment>
-          <UserForm title='Registrarse' />
-          <UserForm title='Iniciar sesión' />
+          <RegisterMutation>
+            {(register) => {
+              const onSubmit = ({ email, password }) => {
+                const input = { email, password };
+                const variables = { input };
+                register({ variables }).then(activateAuth);
+              };
+
+              return <UserForm title='Registrarse' onSubmit={onSubmit} />;
+            }}
+          </RegisterMutation>
+
+          <UserForm title='Iniciar sesión' onSubmit={activateAuth} />
         </Fragment>
       );
     }}
