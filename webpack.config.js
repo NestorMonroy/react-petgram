68e152d0927d4cf7e1c2a46444d207b1b005ea0a
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-03-19 14:34:18 -0600

    [initial] PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifestPlugin = require("webpack-pwa-manifest");
 
 module.exports = {
   output: {
@@ -17,6 +18,37 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
+    }),
+    new WebpackPwaManifestPlugin({
+      filename: 'manifest.webmanifest',
+      name: "Pettagram - Tu app social de mascotas",
+      short_name: "Pettagram",
+      description:
+        "Con Petgram puedes encontrar y subir fotos de animales domésticos.",
+      background_color: "#ffffff",
+      theme_color: "#2196f3",
+      crossorigin: "use-credentials", //can be null, use-credentials or anonymous
+      orientation: 'portrait',
+      display: 'standalone',
+      start_url: '/',
+      scope: '/',
+      icons: [
+        {
+          src: path.resolve("src/assets/icon1.png"),
+          sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+          purpose: "maskable",
+          ios: true,
+        },
+        {
+          src: path.resolve("src/assets/icon1.png"),
+          size: "1024x1024", // you can also use the specifications pattern
+        },
+        {
+          src: path.resolve("src/assets/icon1.png"),
+          size: "1024x1024",
+          purpose: "maskable",
+        },
+      ],
     }),
   ],
   module: {
